96e8f719a30495258ca4912d91a03f482e10aa76
seoyoungchae0415 <seoyoungchae0415@gmail.com>
2025-02-12 20:10:47 -0600

    still here

---
+++ codepractice/hw.cpp
@@ -0,0 +1,13 @@
+#include <iostream>
+#include <stdio.h>
+
+using namespace std;
+
+int main() {
+    int n = 6;
+    int  cray = 9;
+    printf("%d", n + cray);
+    cout << "TEST";
+
+    return 0;
+}
